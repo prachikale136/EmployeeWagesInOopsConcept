a29bc347be1341cea1a2058719b6866f6a302e06
Prachi <prachikale136@gmail.com>
2023-02-25 17:22:20 +0530

    [Prachi] Add: Add the part Time Employee

--- EmployeeWages/EmployeeWages/EmployeePayRoll.cs
+++ EmployeeWages/EmployeeWages/EmployeePayRoll.cs
@@ -9,21 +9,27 @@ namespace EmployeeWages
     internal class EmployeePayRoll
     {
         public const int WAGEPERHRS = 20;
-        public const int FULL_DAY_PERHR = 8;
-        public const int PART_TIME_HRS = 4;
+        //public const int FULL_DAY_PERHR = 8;
+        //public const int PART_TIME_HRS = 4;
+       
         public static int EmployeeAttendance()
         {
-          const int IS_FULL_TIME = 1;
+             int IS_FULL_TIME = 1;
+             int IS_PART_TIME = 2;
             int EmpHrs = 0;
            Random random = new Random();
-            int check = random.Next(0 , 2);
+            int check = random.Next(0 , 3);
             if (check == IS_FULL_TIME)
             {
                 EmpHrs = 8;
             }
-            else
+            else if (check == IS_PART_TIME)
             {
-                EmpHrs= 4;
+                EmpHrs = 4;
+            }
+            else
+            { 
+                EmpHrs= 0;
             }
             int daliyWage = EmpHrs * WAGEPERHRS;
             return daliyWage;
