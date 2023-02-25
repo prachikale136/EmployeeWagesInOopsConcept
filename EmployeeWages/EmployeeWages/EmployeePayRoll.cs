65e8098f5358818095b2378c5317498043cc2461
Prachi <prachikale136@gmail.com>
2023-02-25 16:54:41 +0530

    [PRachi]

--- EmployeeWages/EmployeeWages/EmployeePayRoll.cs
+++ EmployeeWages/EmployeeWages/EmployeePayRoll.cs
@@ -8,22 +8,35 @@ namespace EmployeeWages
 {
     internal class EmployeePayRoll
     {
-        
-        public static void EmployeeAttendance()
+        public const int WAGEPERHRS = 20;
+        public const int FULL_DAY_PERHR = 8;
+        public static int EmployeeAttendance()
         {
           const int IS_FULL_TIME = 1;
-        Random random = new Random();
-            int check = random.Next(0 , 2);
-            if (check == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is present");
-            }
-            else 
-            {
-                Console.WriteLine("Employee is absent");
-            }
-
-                   
+           Random random = new Random();
+            int check = random.Next(0 , 3);
+            return check;
         }
+
+        public static int EmployeeDailyWage() 
+        {
+            int check = EmployeeAttendance();
+            int empHrs = 0;
+            switch (check)
+            {
+                case 0 :
+                    empHrs = 0;
+                    break;
+                case 1 :
+                    empHrs = 4;
+                    break;
+                case 2 :
+                    empHrs = 8;
+                    break;
+            }
+            int daliyWage = empHrs * WAGEPERHRS;
+            return daliyWage;
+        }
+
      }
 }
--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-           EmployeePayRoll.EmployeeAttendance();
-            
+         int emp =   EmployeePayRoll.EmployeeDailyWage();
+            Console.WriteLine(emp);
         }
     }
 }
