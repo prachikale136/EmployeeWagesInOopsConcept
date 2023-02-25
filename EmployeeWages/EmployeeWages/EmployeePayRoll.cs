f275fdee6db8e3612e6863da88219d4fb9d813e4
Prachi <prachikale136@gmail.com>
2023-02-25 17:10:56 +0530

    [Prachi] Add: Calculate daily wages

--- EmployeeWages/EmployeeWages/EmployeePayRoll.cs
+++ EmployeeWages/EmployeeWages/EmployeePayRoll.cs
@@ -10,33 +10,26 @@ namespace EmployeeWages
     {
         public const int WAGEPERHRS = 20;
         public const int FULL_DAY_PERHR = 8;
+        public const int PART_TIME_HRS = 4;
         public static int EmployeeAttendance()
         {
           const int IS_FULL_TIME = 1;
+            int EmpHrs = 0;
            Random random = new Random();
-            int check = random.Next(0 , 3);
-            return check;
-        }
-
-        public static int EmployeeDailyWage() 
-        {
-            int check = EmployeeAttendance();
-            int empHrs = 0;
-            switch (check)
+            int check = random.Next(0 , 2);
+            if (check == IS_FULL_TIME)
             {
-                case 0 :
-                    empHrs = 0;
-                    break;
-                case 1 :
-                    empHrs = 4;
-                    break;
-                case 2 :
-                    empHrs = 8;
-                    break;
+                EmpHrs = 8;
             }
-            int daliyWage = empHrs * WAGEPERHRS;
+            else
+            {
+                EmpHrs= 4;
+            }
+            int daliyWage = EmpHrs * WAGEPERHRS;
             return daliyWage;
         }
+
+        
 
      }
 }
--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-         int emp =   EmployeePayRoll.EmployeeDailyWage();
+         int emp =   EmployeePayRoll.EmployeeAttendance();
             Console.WriteLine(emp);
         }
     }
