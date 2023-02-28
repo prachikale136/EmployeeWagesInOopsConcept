caf6471e2db212f98139347641075bfeb656247f
Prachi <prachikale136@gmail.com>
2023-03-01 00:39:40 +0530

    [Prachi] Add: Ability to save Total Wage for comapny

--- EmployeeWages/EmployeeWages/EmployeePayRoll.cs
+++ EmployeeWages/EmployeeWages/EmployeePayRoll.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,14 +12,28 @@ namespace EmployeeWages
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
 
-      public static int ComputeEmployeeWages(string company, int empRatePerHr, int numberOfWorkingDay, int maxHrsPerMonth)
-      {
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
-        int empHrs = 0;
+        private string company;
+        private int empRatePerHrs;
+        private int numOfWorkingDays;
+        private int maxHrsPerMonth;
+        private int totalEmpWage;
 
-            while (totalEmpHrs <= maxHrsPerMonth && totalWorkingDays < numberOfWorkingDay)
-            { 
+        public EmployeePayRoll(string company, int empRatePerHr, int numOfWorkingDays, int maxHrsPerMonth) 
+        {
+            this.company = company;
+            this.empRatePerHrs= empRatePerHr; 
+            this.numOfWorkingDays = numOfWorkingDays;  
+            this.maxHrsPerMonth = maxHrsPerMonth;
+        }
+
+        public void ComputeEmployeeWages()
+        {
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            int empHrs = 0;
+
+            while (totalEmpHrs <= this.maxHrsPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
                 totalWorkingDays++;
                 Random random = new Random();
                 int check = random.Next(0, 3);
@@ -37,13 +52,14 @@ namespace EmployeeWages
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Days:" + totalWorkingDays + "emp hrs" + empHrs);
             }
-             int totalEmpWage = totalEmpHrs * empRatePerHr;
+            int totalEmpWage = totalEmpHrs * this.empRatePerHrs;
             Console.WriteLine("total emp wage for company: " + company + "is:" + totalEmpWage);
-            return totalEmpWage;
-     }
-        
-                    
-    }
+            //return totalEmpWage;
 
-    
+        }
+            public string toString()
+            {
+                return "total emp wage for company: " + this.company + " is:" + this.totalEmpWage;
+            }
+     }
 }
--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -4,9 +4,17 @@
     {
         static void Main(string[] args)
         {
-         EmployeePayRoll.ComputeEmployeeWages("Dmart", 20, 2, 10);
-            EmployeePayRoll.ComputeEmployeeWages("Reliance", 10, 4, 20);
-          
+         //EmployeePayRoll.ComputeEmployeeWages("Dmart", 20, 2, 10);
+           // EmployeePayRoll.ComputeEmployeeWages("Reliance", 10, 4, 20);
+            EmployeePayRoll dMart = new EmployeePayRoll("Dmart", 20, 2, 10);
+            EmployeePayRoll reliance = new EmployeePayRoll("Reliance", 10, 4, 20);
+
+            dMart.ComputeEmployeeWages();
+            Console.WriteLine(dMart.toString());
+            reliance.ComputeEmployeeWages();
+            Console.WriteLine(reliance.toString());
+
+
         }
     }
 }
